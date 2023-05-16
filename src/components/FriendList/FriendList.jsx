934face9694c355b032b1016f3a8cfd33e5bbf1c
lllastun <lllastun@gmail.com>
2023-05-17 02:12:29 +0300

    16

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class FriendList extends Component {
+  render() {
+    return (
+      <div>FriendList</div>
+    )
+  }
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div>Profiledsadsd</div>
+    )
+  }
+}
+
+// import React from 'react'
+
+// const Profile = () => {
+//   return (
+//     <div>Profile</div>
+//   )
+// }
+
+// export default Profile
+
+// import React, { Component } from 'react'
+
+// export default class Profile extends Component {
+//   render() {
+//     return (
+//       <div>Profile</div>
+//     )
+//   }
+// }
+
+// rcc
